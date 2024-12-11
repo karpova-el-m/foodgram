124ddae3ed0e8ebd281a825772f58a8ae215fa3c
Елена Карпова <karpova.el.m@gmail.com>
2024-12-11 11:24:24 +0100

    New static

--- backend/foodgram_project/settings.py
+++ backend/foodgram_project/settings.py
@@ -3,8 +3,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 dotenv_path = os.path.join(BASE_DIR, '.env')
 
